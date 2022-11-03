32c9f3b42ca02e01bd886f55afbe1aee37f6f754
Tuomas Poukkula <66461922+tuopouk@users.noreply.github.com>
2022-11-04 00:26:27 +0200

    Update keyfiguresfinland.py

--- keyfiguresfinland.py
+++ keyfiguresfinland.py
@@ -6,49 +6,29 @@ import plotly.express as px
 import orjson
 from plotly.io.json import to_json_plotly
 
-# municipal_data = pd.read_csv('assets/key_figures_municipalities.csv', encoding = 'latin-1').rename(columns ={'Region 2021':'Municipality'}).set_index('Municipality')
+# Data provided by Statistics Finland.
 regions_data = pd.read_csv('assets/key_figures_regions.csv', encoding = 'latin-1').rename(columns ={'Region 2021':'Region'}).set_index('Region')
-# subregions_data = pd.read_csv('assets/key_figures_subregions.csv', encoding = 'latin-1').rename(columns ={'Region 2021':'Sub-region'}).set_index('Sub-region')
+
+# Whole country figures on a pandas Series.
 whole_country_df = regions_data.loc['WHOLE COUNTRY']
 
-# The municipal dataset also has the whole country's key figures.
+# Drop since not needed.
 regions_data.drop('WHOLE COUNTRY', axis = 0, inplace = True)
 
-# https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta1000k_2021&outputFormat=json
-# with open('assets/municipalities.json', encoding = 'ISO-8859-1') as f:
-#     municipalities_json = orjson.loads(f.read())
-    
+# The json file for the mapbox viz.
 # https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=maakunta1000k_2021&outputFormat=json
 with open('assets/regions.json', encoding = 'ISO-8859-1') as f:
     regions_json = orjson.loads(f.read())
-
-# https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:seutukunta1000k_2021&outputFormat=json    
-# with open('assets/sub-regions.json', encoding = 'utf-8') as f:
-#     subregions_json = orjson.loads(f.read())
-    
-geojson_collection = {
-    # 'Municipality':municipalities_json, 
-    'Region':regions_json, 
-#     'Sub-region': subregions_json
-}
-data_collection =  {
-    # 'Municipality':municipal_data, 
-    'Region':regions_data, 
-#     'Sub-region': subregions_data
-}
 
 key_figures = sorted(list(pd.unique(regions_data.columns)))
 
 external_stylesheets = [dbc.themes.SUPERHERO]
 
-app = Dash(name = __name__, external_stylesheets = external_stylesheets
-          )
+app = Dash(name = __name__, external_stylesheets = external_stylesheets)
 app.title = "Finland's Regional Key Figures"
 server = app.server
 
-def serve_layout():
-    
-    return dbc.Container([
+app.layout = dbc.Container([
         
         html.Div("Finland's Regional Key Figures",style={'textAlign':'center'}, className="mb-3 mt-3 fw-bold display-1"),
 
@@ -63,13 +43,6 @@ def serve_layout():
                              style = {'font-size':20, 'font-family':'Arial','color': 'black'}
                              ),
                 html.Br(),
-#                 html.H2('Regional level'),
-#                 dcc.Dropdown(id = 'key-figures-finland-region-selection-x',
-#                              options = [{'label':region, 'value':region} for region in data_collection.keys()],
-#                              value = 'Region',
-#                              multi = False,
-#                              style = {'font-size':20, 'font-family':'Arial','color': 'black'}
-#                              ),
                 html.H1(id = 'key-figures-finland-whole-country-header-x', style = {'textAlign':'center'}, className="mt-5 display-2"),
                 html.Div(['Data by ',html.A('Statistics Finland', href = 'https://pxdata.stat.fi/PxWeb/pxweb/en/Kuntien_avainluvut/Kuntien_avainluvut__2021/kuntien_avainluvut_2021_viimeisin.px/', target = '_blank')], className="text-center fs-3 text"),
                 
@@ -81,21 +54,21 @@ def serve_layout():
                 ], xs = 12, sm = 12, md = 12, lg = 6, xl = 6, xxl = 6)
 
             ], justify = 'center', className = "m-auto d-flex justify-content-center"),
-        dcc.Store(id = 'key-figures-finland-map-data-store-x'),
-        dcc.Store(id = 'key-figures-finland-map-layout-store-x')        
+    
+        dcc.Store(id = 'key-figures-finland-map-data-store-x'), # Store map data.
+        dcc.Store(id = 'key-figures-finland-map-layout-store-x') # Store map layout.       
         ], fluid = True)    
 
+# Empty map for initialization.
 def plot_empty_map():
     return px.choropleth_mapbox(center = {"lat": 64.961093, "lon": 27.590605})
 
-def plot_map(key_figure, region_level):
-    
-    df = data_collection[region_level]
-    geojson = geojson_collection[region_level]
-    
-    fig = px.choropleth_mapbox(df[[key_figure]].reset_index(), 
-                           geojson=geojson, 
-                           locations=region_level, 
+# Generate map data and layout.
+def plot_map(key_figure):
+
+    fig = px.choropleth_mapbox(region_data[[key_figure]].reset_index(), 
+                           geojson=region_json, 
+                           locations='Region', 
                            color=key_figure,
                            mapbox_style="open-street-map",
                            featureidkey='properties.name',
@@ -134,7 +107,8 @@ def update_map(key_figure):
     map_layout = orjson.loads(to_json_plotly(map_figure))['layout']
     
     return map_data, map_layout    
-   
+
+# Update map on clientside.
 app.clientside_callback(
 
     """
@@ -150,6 +124,6 @@ app.clientside_callback(
     [Input('key-figures-finland-map-data-store-x','data'),
     Input('key-figures-finland-map-layout-store-x','data')]
 )
-app.layout = serve_layout
+
 if __name__ == "__main__":
     app.run_server(debug=False)    
