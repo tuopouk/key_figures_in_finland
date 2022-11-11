f91da60b8917609f9e1e49e38155e2688dccf23f
Tuomas Poukkula <66461922+tuopouk@users.noreply.github.com>
2022-11-11 07:36:47 +0200

    Update keyfiguresfinland.py

--- keyfiguresfinland.py
+++ keyfiguresfinland.py
@@ -1288,7 +1288,7 @@ app.layout = dbc.Container([
                         html.H3('Change map type', className = 'mt-2'),
                         dcc.Dropdown(id = 'key-figures-finland-map-type-x', 
                                      options = ["open-street-map", "carto-positron", "carto-darkmatter", "stamen-terrain", "stamen-toner" ,"stamen-watercolor"],
-                                     value = "stamen-terrain",
+                                     value = "carto-positron",
                                      multi = False,
                                      style = {'fontSize':'1.2rem', 'color': 'black','whiteSpace': 'nowrap'})
                         ]),
